c737bbaf9d009549a936fed2abe4ea2a62d20422
Grant <116581445+grantonthenet@users.noreply.github.com>
2023-03-03 02:06:29 -0600

    Create pipe.lua

---
+++ pipe.lua
@@ -0,0 +1,25 @@
+-- moves items from multiple source inventories to one destination
+local sources = {
+    "minecraft:barrel_197",
+    "minecraft:barrel_198",
+    "minecraft:barrel_199",
+    "minecraft:barrel_200",    
+}
+local dst = "sc-goodies:iron_barrel_125"
+
+while true do
+    sleep(0)
+    -- go through sources
+    for si=1,#sources do
+        -- wrap source, list items
+        local s = peripheral.wrap(sources[si])
+        local l = s.list()
+        -- move items to destination
+        for ii=1,#l do
+            -- move if item in slot
+            if l[ii] then
+                s.pushItems(dst,ii)
+            end
+        end
+    end
+end
