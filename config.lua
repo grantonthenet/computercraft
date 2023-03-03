be0905bee4515a063f0ba1e75ed06e5e4d043147
Grant <116581445+grantonthenet@users.noreply.github.com>
2023-03-03 02:13:38 -0600

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,49 @@
+config = {}
+
+config.storage = {
+    smelting = {
+        copper = {
+            input  = "minecraft:barrel_218",
+            output = "minecraft:barrel_217",
+            furnaces = {
+                2694,
+                2693,
+                2692,
+                2695,
+                2696,
+                2691,
+                2690,
+                2697
+            }
+        },
+        iron = {
+            input  = "minecraft:barrel_221",
+            output = "minecraft:barrel_222",
+            furnaces = {
+                2705,
+                2703,
+                2701,
+                2699,
+                2704,
+                2702,
+                2700,
+                2698
+            }
+        },
+        gold = {
+            input  = "minecraft:barrel_219",
+            output = "minecraft:barrel_220",
+            furnaces = {
+                2713,
+                2711,
+                2709,
+                2707,
+                2712,
+                2710,
+                2708,
+                2706
+            }
+        }
+    },
+    coal = "minecraft:barrel_216"
+}
